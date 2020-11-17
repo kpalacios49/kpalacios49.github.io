33d0dbda2104f9f8f1e89ac8b50ce1e3e665b574
Kevin <33708864+kevin499@users.noreply.github.com>
2020-11-17 13:26:39 -0300

    added lottie animations

---
+++ resources/lottie.js
@@ -0,0 +1,42 @@
+let scrollDown = bodymovin.loadAnimation({
+    container: document.getElementById("scrollDown"),
+    rendered: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'resources/images/scrollDown.json'
+})
+scrollDown.setSpeed(1.5)
+
+let menu = bodymovin.loadAnimation({
+    container: document.getElementById("hamburguer"),
+    rendered: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'resources/images/menuV4.json'
+
+})
+let menu_activated = true;
+document.getElementById("hamburguer").addEventListener('click',function(){
+    if (menu_activated){
+        menu_activated = false;
+        menu.setDirection(1); menu.play();
+    }
+    else{
+        menu_activated = true;
+        menu.setDirection(-1); menu.play();
+    }
+})
+
+
+
+
+/*
+/!* Play an animation on each click *!/
+var hamburger= document.getElementById('menu');
+hamburger.addEventListener('click', function(){
+    hamburger = !hamburger if(hamburger) {
+        console.log('Second Click');
+
+    }
+    else { console.log('First click'); menu.setDirection(1); menu.play(); }
+})*/
