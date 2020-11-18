b64799f7bf2deb937a8f3a0b326b3d8c545b62cf
Kevin <33708864+kevin499@users.noreply.github.com>
2020-11-18 17:58:35 -0300

    adding proyect cards

--- resources/app.js
+++ resources/app.js
@@ -1,1 +1,13 @@
 AOS.init();
+
+document.body.addEventListener("click", (e)=>{
+    let header = document.querySelector("header");
+    console.log();
+    if (!header.contains(e.target)){
+        menu_activated = true;
+        menu.setDirection(-1);
+        menu.play();
+        menu_left.classList.add("disappear")
+
+    }
+})
