1215a66717ba4711944d17c49b3ab33eaeb63d4e
Kevin <33708864+kevin499@users.noreply.github.com>
2020-12-15 22:37:44 -0300

    added menu swipe

--- resources/app.js
+++ resources/app.js
@@ -11,3 +11,41 @@ document.body.addEventListener("click", (e)=>{
 
     }
 })
+
+
+var touchsurface = document.getElementById('touchsurface'),
+    startX,
+    startY,
+    dist,
+    threshold = 150,
+    allowedTime = 200,
+    elapsedTime,
+    startTime
+
+function handleswipe(isrightswipe){
+    if (isrightswipe){
+        menu_activated = false;
+        menu.setDirection(1);
+        menu.play();
+        menu_left.classList.remove("disappear")
+    }
+}
+touchsurface.addEventListener('touchstart', function(e){
+    var touchobj = e.changedTouches[0]
+    dist = 0
+    startX = touchobj.pageX
+    startY = touchobj.pageY
+    startTime = new Date().getTime()
+}, false)
+
+touchsurface.addEventListener('touchmove', function(e){
+}, false)
+
+touchsurface.addEventListener('touchend', function(e){
+    var touchobj = e.changedTouches[0]
+    dist = touchobj.pageX - startX
+    elapsedTime = new Date().getTime() - startTime // get time elapsed
+    var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+    handleswipe(swiperightBol)
+}, false)
+
